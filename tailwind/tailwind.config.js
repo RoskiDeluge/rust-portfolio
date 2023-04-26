9bd56e743462c693012672bcb9a5832c7f82b0f4
RoskiDeluge <roberto.delgado@gmail.com>
2023-04-25 22:13:08 -1000

    Added post.css workaround

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,3 +1,20 @@
+// Manually disabled code block CSS for tailwind
+// because we wish to style this using some custom css
+// that works on markdown output without the tailwind
+// typography plugin. Reference for disabling:
+// https://building.hellonext.co/blog/disable-tailwind-prose-code
+const disabledCss = {
+  "code::before": false,
+  "code::after": false,
+  "blockquote p:first-of-type::before": false,
+  "blockquote p:last-of-type::after": false,
+  pre: false,
+  code: false,
+  "pre code": false,
+  "code::before": false,
+  "code::after": false,
+};
+
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
